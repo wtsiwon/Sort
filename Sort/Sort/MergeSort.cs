a4947e87294ca722868d9ad398331c7bd3391516
wtsiwon <siwon4727@gmail.com>
2023-04-16 01:20:53 +0900

    .

--- Sort/Sort/MergeSort.cs
+++ Sort/Sort/MergeSort.cs
@@ -11,10 +11,17 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            int count = int.Parse(Console.ReadLine());
-            Console.WriteLine(Func(count));
-        }
+            int[] arr = new int[10] { 1, 6, 2, 8, 5, 4, 3, 9, 10, 7 };
 
+            SortArray(arr, 0, arr.Length - 1);
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        //재귀함수 연습
         static string Func(int count)
         {
             int[] arr = new int[count];
@@ -36,22 +43,66 @@ namespace Sort
             return "None";
         }
 
-
-        static void MergeSortFunc<T>(this IEnumerable<T> arr, int startIndex, int endIndex)
+        /// <summary>
+        /// 정렬
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static int[] SortArray(int[] array, int left, int right)
         {
-            if (endIndex == 0)
+            if (left < right)
             {
-                return;
+                int middle = left + (right - left) / 2;
+
+                SortArray(array, left, middle);//앞부터 중간까지
+                SortArray(array, middle + 1, right);//중간부터 끝까지
+
+                MergeArray(array, left, middle, right);//합치기~
             }
 
+            return array;
         }
 
-        static void Merge(int[] arr, int startIndex, int midIndex, int endIndex)
+        public static void MergeArray(int[] array, int left, int middle, int right)
         {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
+            int i, j;
 
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+
+            i = 0;
+            j = 0;
+            int k = left;
+
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
+            }
+
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
         }
-
-
-
     }
 }
